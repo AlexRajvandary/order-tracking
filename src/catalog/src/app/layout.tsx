704dda00aea1c7e1ffec468d3e33011f287de14d
stark <stark.11raj@gmail.com>
2026-09-14 13:47:18 +0100

    fix metrika

--- src/catalog/src/app/layout.tsx
+++ src/catalog/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Suspense } from "react";
 import { CartProvider } from "@/components/cart-provider";
 import { FavoritesProvider } from "@/components/favorites-provider";
@@ -22,10 +23,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru" className="h-full">
+      <head>
+        <Script id="yandex-metrika" strategy="beforeInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m.dataLayer=m.dataLayer||[];
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a);
+            })(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=112574449','ym');
+
+            if(!window.__theGetMetrikaInitialized){
+              ym(112574449,'init',{ssr:true,defer:true,webvisor:true,clickmap:true,ecommerce:'dataLayer',accurateTrackBounce:true,trackLinks:true});
+              ym(112574449,'hit',location.href,{referer:document.referrer,title:document.title});
+              window.__theGetMetrikaInitialized=true;
+              window.__theGetMetrikaLastUrl=location.href;
+            }
+          `}
+        </Script>
+      </head>
       <body className="flex min-h-full flex-col bg-background font-sans antialiased">
         <Suspense fallback={null}>
           <YandexMetrika />
         </Suspense>
+        <noscript>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://mc.yandex.ru/watch/112574449"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
         <CartProvider>
           <FavoritesProvider>
             <main className="flex min-h-full flex-1 flex-col bg-background">{children}</main>
--- src/catalog/src/components/yandex-metrika.tsx
+++ src/catalog/src/components/yandex-metrika.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Script from "next/script";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
@@ -37,43 +36,5 @@ export function YandexMetrika() {
     window.__theGetMetrikaLastUrl = url;
   }, [pathname, query]);
 
-  function initialize() {
-    window.dataLayer = window.dataLayer || [];
-    if (!window.ym || window.__theGetMetrikaInitialized) return;
-
-    window.ym(COUNTER_ID, "init", {
-      ssr: true,
-      webvisor: true,
-      clickmap: true,
-      ecommerce: "dataLayer",
-      referrer: document.referrer,
-      url: currentUrl(),
-      accurateTrackBounce: true,
-      trackLinks: true,
-    });
-    window.__theGetMetrikaInitialized = true;
-    window.__theGetMetrikaLastUrl = currentUrl();
-  }
-
-  return (
-    <>
-      <Script
-        id="yandex-metrika"
-        src={`https://mc.yandex.ru/metrika/tag.js?id=${COUNTER_ID}`}
-        strategy="afterInteractive"
-        onLoad={initialize}
-        onReady={initialize}
-      />
-      <noscript>
-        <div>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src={`https://mc.yandex.ru/watch/${COUNTER_ID}`}
-            style={{ position: "absolute", left: "-9999px" }}
-            alt=""
-          />
-        </div>
-      </noscript>
-    </>
-  );
+  return null;
 }
