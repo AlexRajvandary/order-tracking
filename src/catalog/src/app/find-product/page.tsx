5703d5c72ebb66dbb3693b2bcd3f410327d6f2f0
stark <stark.11raj@gmail.com>
2026-09-15 02:24:59 +0100

    fix

--- src/catalog/src/app/find-product/page.tsx
+++ src/catalog/src/app/find-product/page.tsx
@@ -17,7 +17,6 @@ export default function FindProductPage() {
         <p className="font-medium text-foreground">Что можно указать:</p>
         <ul className="mt-2 list-disc space-y-1 pl-5">
           <li>какой товар вам нужен;</li>
-          <li>для чего вы планируете его использовать;</li>
           <li>желаемый бюджет;</li>
           <li>важные характеристики, бренд, размер, цвет или другие пожелания;</li>
           <li>примеры или фотографии того, что вам нравится.</li>
